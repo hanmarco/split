184b92c18e563105c38fdde11e70df0bdb35afb9
Marco <hsn103@gmail.com>
2024-10-13 04:37:28 +0900

    chore(config): configure vue.config.js for PWA and deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/split/' : '/',
+  pwa: {
+    name: 'Split',
+    workboxOptions: {
+      navigateFallback: '/split/index.html',
+    }
+  },
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
