63dcd680d12fb5af4a9160a661058ce5d603dad0
Marco <hsn103@gmail.com>
2024-10-15 01:11:19 +0900

    teest

--- src/components/MainPanel.vue
+++ src/components/MainPanel.vue
@@ -29,11 +29,11 @@ export default createVuetify({
     <v-container>
       <v-row>
         <v-col cols="12" sm="6">
-          <v-card class="pa-2" elevation="16">
-            <v-text-field label="참가자 이름" v-model="newPerson" @keyup.enter="addPerson" placeholder="ex)홍길동 철수 영희" append-inner-icon="mdi-plus" @click:append-inner="addPerson" />
+          <v-card class="pa-4" elevation="16">
+            <v-text-field label="참가자 이름" v-model="newPerson" hide-details="auto" @keyup.enter="addPerson" placeholder="ex)홍길동 철수 영희" append-inner-icon="mdi-plus" @click:append-inner="addPerson" />
             <!-- <v-btn v-if="people.length > 0" @click="removeAllPeople" class="float-right" color="warning">Clear All</v-btn> -->
 
-            <v-card class="mx-auto" v-if="people.length > 0">
+            <v-card class="mx-auto mt-4" v-if="people.length > 0">
               <v-toolbar color="deep-purple-accent-4" density="compact">
                 <v-toolbar-title>모임 멤버</v-toolbar-title>
                 <v-btn icon="mdi-close" @click="removeAllPeople"></v-btn>
@@ -51,15 +51,17 @@ export default createVuetify({
       <v-row v-if="people.length>0">
         <v-col cols="12" sm="6">
           <v-card class="pa-2" elevation="16">
-            <v-btn class="ma-1 float-right" color="secondary" @click="openPaymentDialog">결제 추가</v-btn>
+
+              <v-btn block class="" color="secondary" @click="openPaymentDialog">결제 추가</v-btn>
+
             <v-dialog v-model="isPaymentDialogOpen" max-width="500px">
               <v-card>
                 <v-card-title>{{ isEditMode ? '결제 수정' : '결제 추가' }}</v-card-title>
                 <v-card-text>
-                  <v-text-field label="모임 제목" v-model="newPayment.title" prepend-icon="mdi-account-group" />
-                  <v-text-field label="결제 금액" v-model.number="newPayment.amount" type="number" prefix="₩" prepend-icon="mdi-cash" />
+                  <v-text-field label="모임 제목(선택)" placeholder="꼭 입력하지 않아도 됩니다" v-model="newPayment.title" prepend-icon="mdi-account-group" />
+                  <v-text-field label="결제 금액(필수)" v-model.number="newPayment.amount" type="number" prefix="₩" prepend-icon="mdi-cash" />
                   <v-select
-                    label="결제자 선택"
+                    label="결제자 선택(필수)"
                     v-model="newPayment.payer"
                     :items="people"
                     prepend-icon="mdi-account-cash" />
@@ -70,7 +72,7 @@ export default createVuetify({
                     multiple
                     chips
                     prepend-icon="mdi-account-star" />
-                  <v-text-field label="시간 (예: 2024-10-13 14:00)" v-model="newPayment.time" type="datetime-local" prepend-icon="mdi-calendar-clock" />
+                  <v-text-field label="시간(선택)" v-model="newPayment.time" type="datetime-local" prepend-icon="mdi-calendar-clock" />
                 </v-card-text>
                 <v-card-actions>
                   <v-btn @click="closePaymentDialog">취소</v-btn>
@@ -78,7 +80,7 @@ export default createVuetify({
                 </v-card-actions>
               </v-card>
             </v-dialog>
-            <v-list v-if="payments.length>0" class="ma-1">
+            <v-list v-if="payments.length>0" class="ma-1" variant>
               <v-list-item v-for="(payment, index) in payments" :key="index">
                 <v-list-item-content>
                   <v-list-item-title>{{ payment.title }}</v-list-item-title>
