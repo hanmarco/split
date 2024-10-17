38405c9b3ad43abbc535c0e2eab2a6210953c6c1
Marco <hsn103@gmail.com>
2024-10-17 23:24:31 +0900

    vue add pwa

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import './registerServiceWorker'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .mount('#app')
-  
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/split/service-worker.js', { scope: '/split/' })
   .then(function(registration) {
