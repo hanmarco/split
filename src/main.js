5f327d3ff5f6ec28c7bfe89c2a7fb91662218652
Marco <hsn103@gmail.com>
2024-10-17 23:34:05 +0900

    Remove previous config

--- src/main.js
+++ src/main.js
@@ -9,13 +9,3 @@ loadFonts()
 createApp(App)
   .use(vuetify)
   .mount('#app')
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/split/service-worker.js', { scope: '/split/' })
-  .then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-  })
-  .catch(function(error) {
-      console.error('Service Worker registration failed:', error);
-  });
-}
