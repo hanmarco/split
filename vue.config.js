486fa8bbd983a3f9a899eabcb1dca651c60bc4da
Marco <hsn103@gmail.com>
2024-10-17 23:57:44 +0900

    Fix some json

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/split/' : '/',
-  pwa: {
-    name: 'Split',
-    workboxOptions: {
-      navigateFallback: '/split/index.html',
-    },
-    manifestOptions: {
-      start_url: '/split/',
-      scope: '/split/',
-      display: 'standalone',
-      background_color: '#ffffff'
-    },
-  },
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {
